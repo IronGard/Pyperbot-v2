e23d5fc8c40464e8bded2a2aa15ac967a9d2fa9d
IronGard <hahnlon.lam@gmail.com>
2024-03-02 22:19:35 +0000

    merge prep

--- pyperbot_v2/snakebot_description/snakebot_sim.py
+++ pyperbot_v2/snakebot_description/snakebot_sim.py
@@ -4,6 +4,8 @@ import os
 import time
 import pybullet_data
 import sys
+import numpy as np
+import pandas as pd
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.join(current_dir, '..')
@@ -19,6 +21,7 @@ args = parse_args()
 sim_env = args.env
 gait_type = args.gait
 num_goals = args.num_goals
+timesteps = args.timesteps
 
 def main():
     pyb_setup = Loader("--mp4=results/videos/training_video.mp4")
@@ -33,19 +36,32 @@ def main():
         print('No selected environment.')
     
     robot_id = pyb_setup.robot("pyperbot_v2/snakebot_description/urdf/full_snakebot_no_macro.urdf.xacro")
-
+    moving_joints = [p.getJointInfo(robot_id, joint) for joint in range(p.getNumJoints(robot_id)) if p.getJointInfo(robot_id, joint)[2] != p.JOINT_FIXED]
+    moving_joint_ids = [p.getJointInfo(robot_id, joint)[0] for joint in range(p.getNumJoints(robot_id)) if p.getJointInfo(robot_id, joint)[2] != p.JOINT_FIXED]
+    num_moving_joints = len(moving_joints)
     pyb_gaits = Gaits(robot_id)
 
-    while True:
+    joint_pos = []
+    for i in range(args.timesteps):
         pyb_gaits.lateral_undulation()
         #pyb_gaits.concertina_locomotion()
         #_, ori = p.getBasePositionAndOrientation(robot_id)
         #euler = p.getEulerFromQuaternion(ori)
         #print(euler)
         #pyb_setup.camera()
+        pos, ori = p.getBasePositionAndOrientation(robot_id)
+        euler = p.getEulerFromQuaternion(ori)
+        print("Base position: ", pos)
+        #save joint positions to csv
+        
+        for j in range(len(moving_joint_ids)):
+            joint_pos.append(p.getJointState(robot_id, moving_joint_ids[j])[0])
         p.stepSimulation()
         time.sleep(1/240)
     
+    joint_pos = np.array(joint_pos)
+    df = pd.DataFrame(joint_pos)
+    df.to_csv('pyperbot_v2/results/csv/joint_positions.csv', index = False)
 
 if __name__ == "__main__":
     main()
--- pyperbot_v2/snakebot_description/snakebot_sim_argparse.py
+++ pyperbot_v2/snakebot_description/snakebot_sim_argparse.py
@@ -8,6 +8,7 @@ def parse_args():
     parser.add_argument('-e', '--env', type=str, default='none', help='Sim env: none, maze, lab.')
     parser.add_argument('-g', '--gait', type=str, default='lateral_undulation', help='snake gaits: lateral undulation, concertina_locomotion')
     parser.add_argument('-ng', '--num_goals', type = int, default = 3, help = 'Number of goals (maze env only).')
+    parser.add_argument('-ts', '--timesteps', type = int, default = 2400, help = 'Number of timesteps for the simulation.')
     args = parser.parse_args()
 
     return args
