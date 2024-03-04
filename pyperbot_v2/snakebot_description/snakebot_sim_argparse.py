be3224cbd272742b1d8b8e98f684b8c04c32053d
Hahn Lon Lam <hahnlon.lam@gmail.com>
2024-03-04 13:49:40 +0000

    update to argparse server

--- pyperbot_v2/snakebot_description/snakebot_sim_argparse.py
+++ pyperbot_v2/snakebot_description/snakebot_sim_argparse.py
@@ -10,6 +10,7 @@ def parse_args():
     parser.add_argument('-ng', '--num_goals', type = int, default = 3, help = 'Number of goals (maze env only).')
     parser.add_argument('-c', '--cam', type = int, default = 0, help='Attach head camera: 0, 1.')
     parser.add_argument('-ts', '--timesteps', type = int, default = 2400, help = 'Number of timesteps for the simulation.')
+    parser.add_argument('-s', '--server', type = bool, default = False, help = 'Running code to read data output directly to server')
     args = parser.parse_args()
 
     return args
